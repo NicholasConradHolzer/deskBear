0a7fcf705cf9c9a394aeb3751dbb70738cdbbe24
NicholasCH <nicholasch96@gmail.com>
2022-12-09 15:32:15 -0600

    un smash the react format

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-// const express = require('express');
-// const router = express();
-// const PORT = process.env.PORT || 3001;
+const express = require('express');
+const router = express();
+const PORT = process.env.PORT || 3001;
 
-// const apiRoutes = require('./routes/api-routes');
-// const htmlRoutes = require('./routes/html-routes');
-// const fs= require('fs');
-// const path = require ('path');
+const apiRoutes = require('./routes/api-routes');
+const htmlRoutes = require('./routes/html-routes');
+const fs= require('fs');
+const path = require ('path');
 
-// router.use(express.urlencoded({
-//     extended: true
-// }));
+router.use(express.urlencoded({
+    extended: true
+}));
 
-// router.use(express.static('public'));
-// router.use(express.json());
+router.use(express.static('public'));
+router.use(express.json());
 
-// router.use('/api', apiRoutes);
-// router.use('/', htmlRoutes);
+router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
 
 
-// router.listen(PORT, () => {
-//     console.log(`API server now on port ${PORT}!`);
-// });
+router.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
